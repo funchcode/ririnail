066b1b549df0ec1ca6af7e7f328ce33f262076fd
정채환 <funchcode@gmail.com>
2019-01-20 17:42:46 +0900

    QTableWidget header Size

--- tools/DailyCalendar.py
+++ tools/DailyCalendar.py
@@ -6,15 +6,26 @@ import sys
 class DailyCalendar(QTableWidget):
     def __init__(self):
         super().__init__()
+        self.worker = ["원장님","Test"]
+        self.partTime = ["9:00","9:30","10:00","10:30","11:00","11:30","12:00","12:30",
+                         "13:00", "13:30","14:00","14:30","15:00","15:30","16:00","16:30","17:00","17:30",
+                         "18:00", "18:30","19:00","19:30","20:00","20:30","21:00","21:30","22:00"]
         # self.wg = QWidget()
         # dailyCal = QTableWidget(self.wg)
         # dailyCal.setGeometry(10, 90, 1080, 150)
         # self.setCentralWidget(self.wg)
 
     def makeCalendar(self):
+        options = DailyCalendar()
         wg = QWidget()
         dailyCal = QTableWidget(wg)
-        dailyCal.setGeometry(10, 90, 1080, 150)
+        dailyCal.setGeometry(10, 90, 800, 500)
+        dailyCal.setColumnCount(len(options.worker))
+        dailyCal.setHorizontalHeaderLabels(options.worker)
+        dailyCal.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch) # Header Size 조정(균등하게)
+        dailyCal.resizeColumnToContents(0)
+        dailyCal.setRowCount(len(options.partTime))
+        dailyCal.setVerticalHeaderLabels(options.partTime)
         #self.setCentralWidget(wg)
         return wg
 
